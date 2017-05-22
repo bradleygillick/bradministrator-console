a11d34f05476c929c30b2a441c222c51eee4a7d7
ralphminor <ralphminor@users.noreply.github.com>
2017-05-22 17:04:35 -0600

    Deploy to heroku.

---
+++ server/knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'angular-expenses-crud',
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
+
+};
