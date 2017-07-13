051f64afcf4501053430c9fa8b37cf7ef693b563
Bradley Gillick <bradleygillick@Bradleys-MacBook-Air.local>
2017-07-12 18:42:41 -0600

    Changed integer to bigInteger in migration

--- server/migrations/20170520110722_create-expenses.js
+++ server/migrations/20170520110722_create-expenses.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('hw_values', function (table) {
     table.increments()
     table.string('name')
-    table.integer('value')
+    table.bigInteger('value')
     table.date('date')
   })
 };
